8ae50b98add72977cd28aaad8e80fca7b0743e62
Sabin <skhanal2@tamu.edu>
2022-09-15 15:21:47 -0500

    modified bash file

--- code/get_chnd_all_files.bash
+++ code/get_chnd_all_files.bash
@@ -1,5 +1,6 @@
-#!/usr/bin/env/bash
+#!/bin/bash
 
+#extract file name from archive
 echo "file_name" > data/ghcnd_all_files.txt
 tar tf data/ghcnd_all.tar.gz | grep ".dly" >> data/ghcnd_all_files.txt
 
